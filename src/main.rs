1834c27394c49180ea6797b6b194d7bda6ccb625
Yury Kamenev <yurkam447@gmail.com>
2023-01-11 13:11:01 +0800

    Added progress logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{stderr, Write};
+
 fn main() {
     const IMAGE_WIDTH: u64 = 256;
     const IMAGE_HEIGHT: u64 = 256;
@@ -10,6 +12,9 @@ fn main() {
     println!("{}", WIDTH_DENOM);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Remaining lines: {:3}", j);
+        stderr().flush().unwrap();
+
         for i in 0..IMAGE_WIDTH {
             let r = (i as f64) / WIDTH_DENOM;
             let g = (j as f64) / HEIGHT_DENOM;
@@ -22,4 +27,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+
+    eprintln!("Rendering is completed");
 }
