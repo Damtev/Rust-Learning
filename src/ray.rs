d02206806269f391c97d72602cf7596bbeef724f
Yury Kamenev <yurkam447@gmail.com>
2023-01-22 13:31:50 +0300

    Implemented rendering of gradient sphere

--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,5 @@
 use crate::color::Color;
-use crate::sphere::check_hit_sphere;
+use crate::sphere::hit_sphere;
 use crate::vec3::{Point3, Vec3};
 
 pub struct Ray {
@@ -24,8 +24,11 @@ impl Ray {
     }
 
     pub fn color(&self) -> Color {
-        if check_hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, self) {
-            return Color::new(1.0, 0.0, 0.0);
+        let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, self);
+        if t > 0.0 {
+            let n = (self.at(t) - Vec3::new(0.0, 0.0, -1.0)).normalized();
+
+            return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
         }
 
         let unit_direction = self.direction.normalized();
--- src/sphere.rs
+++ src/sphere.rs
@@ -1,7 +1,7 @@
 use crate::ray::Ray;
 use crate::vec3::Point3;
 
-pub fn check_hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool {
+pub fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin() - center;
 
     let a = ray.direction() * ray.direction();
@@ -10,5 +10,9 @@ pub fn check_hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool {
 
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
