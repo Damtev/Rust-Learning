e57555beebd38cd1ea3e10437908982192f143a3
Yury Kamenev <yurkam447@gmail.com>
2023-01-22 13:33:34 +0300

    Simplified sphere intersection

--- src/sphere.rs
+++ src/sphere.rs
@@ -4,15 +4,15 @@ use crate::vec3::Point3;
 pub fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin() - center;
 
-    let a = ray.direction() * ray.direction();
-    let b = 2.0 * oc * ray.direction();
-    let c = oc * oc - radius * radius;
+    let a = ray.direction().length().powi(2);
+    let half_b = oc * ray.direction();
+    let c = oc.length().powi(2) - radius * radius;
 
-    let discriminant = b * b - 4.0 * a * c;
+    let discriminant = half_b * half_b - a * c;
 
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (-half_b - discriminant.sqrt()) / a
     }
 }
