6c8b8d7d5c2c5982b5c4dd511e42529113efc24f
Yury Kamenev <yurkam447@gmail.com>
2023-01-23 23:39:31 +0300

    Added antialiasing

--- src/color.rs
+++ src/color.rs
@@ -3,11 +3,11 @@ use crate::vec3::Vec3;
 pub type Color = Vec3;
 
 impl Color {
-    pub fn format(self) -> String {
-        format!("{} {} {}",
-                (255.999 * self.x()) as u64,
-                (255.999 * self.y()) as u64,
-                (255.999 * self.z()) as u64
-        )
+    pub fn format(&self, samples_per_pixel: u64) -> String {
+        let ir = (256.0 * (self.x() / (samples_per_pixel as f64)).clamp(0.0, 0.999)) as u64;
+        let ig = (256.0 * (self.y() / (samples_per_pixel as f64)).clamp(0.0, 0.999)) as u64;
+        let ib = (256.0 * (self.z() / (samples_per_pixel as f64)).clamp(0.0, 0.999)) as u64;
+
+        format!("{} {} {}", ir, ig, ib)
     }
 }
