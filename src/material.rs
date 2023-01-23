3595795724732a27a60211ac4832e9d292bd9b15
Yury Kamenev <yurkam447@gmail.com>
2023-01-24 00:49:21 +0300

    Implemented scene with hollow glass sphere

--- src/material.rs
+++ src/material.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use crate::color::Color;
 use crate::hit::HitRecord;
 use crate::ray::Ray;
@@ -69,6 +70,13 @@ impl Dielectric {
             index_of_refraction
         }
     }
+
+    pub fn reflectance(cosine: f64, index_of_refraction: f64) -> f64 {
+        // Use Schlick's approximation for reflectance
+        let r0 = ((1.0 - index_of_refraction) / (1.0 + index_of_refraction)).powi(2);
+
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
 }
 
 impl Scatter for Dielectric {
@@ -83,7 +91,11 @@ impl Scatter for Dielectric {
         let cos_theta = (-1.0 * unit_direction * rec.normal).min(1.0);
         let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
 
-        let direction = if refraction_ratio * sin_theta > 1.0 {
+        let mut random = rand::thread_rng();
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let will_reflect = random.gen::<f64>() < Self::reflectance(cos_theta, refraction_ratio);
+
+        let direction = if cannot_refract || will_reflect {
             unit_direction.reflect(rec.normal)
         } else {
             unit_direction.refract(rec.normal, refraction_ratio)
